95a926b3fd9375778cc185e3bea23d487471ebb7
pawarjay1 <jaykumarpawar08@gmail.com>
2025-08-31 14:23:01 +0530

    commit

---
+++ notes.js
@@ -0,0 +1,28 @@
+// --------------must known topics---------------------------------
+
+// core modules - os, fs, path (basic level knowledge)
+
+// how to create server (different types of requests)
+// how client-server communication works
+
+// how to connect mongodb with nodejs (use mongoose library)- it is schema based model
+// what are models or schema
+
+// body parser in node js 
+
+// what is middleware 
+
+// how api created (we need to create endPoints first with flow diagram).
+
+// parametrized endpoints (/person/:work )
+
+// make express routes 
+
+// make one CRUD operation project not UI Only back end 
+
+// how to connect frontend and backend 
+
+// authentication and autherization 
+// session, cookies and token 
+// what is JWT token and how to authenticate 
+
