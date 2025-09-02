833342eb5de8ff505264f2f97b51922786f796a9
pawarjay1 <jaykumarpawar08@gmail.com>
2025-09-02 23:40:42 +0530

    commit

---
+++ backend_notes/maths.js
@@ -0,0 +1,13 @@
+function addition(a,b){
+    let c = a+b; 
+    return c; 
+}
+
+function subtract(a,b){
+    return a-b; 
+}
+
+module.exports = {
+    addition,
+    subtract
+}
---
+++ backend_notes/server.js
@@ -0,0 +1,5 @@
+const {addition, subtract } = require('./maths'); 
+
+// console.log(subtract(1,2)); 
+
+
